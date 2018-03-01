8206b5b2571236fb8178bab8671c54d6f35ed682
herrychen68 <852088399@qq.com>
2018-03-01 16:33:21 +0800

    dd
    testfrs

---
+++ mytest/mytest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mytest
+//
+//  Created by 雨辰 on 2018/3/1.
+//  Copyright © 2018年 雨辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mytest/mytest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mytest
+//
+//  Created by 雨辰 on 2018/3/1.
+//  Copyright © 2018年 雨辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
